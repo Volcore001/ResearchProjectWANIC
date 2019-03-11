c963dac63e8dbd4f1f6af1d1530694646694a084
William Siauw <36526721+Volcore001@users.noreply.github.com>
2019-03-11 16:49:39 -0700

    Added stuff to my scene
    Added in board & cell scripts

---
+++ WANIC Research Project/Assets/Scripts/Cell.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public struct Cell {
+    public int high;
+    public int medium;
+    public int low;
+
+    public void init()
+    {
+        high = 0;
+        medium = 0;
+        low = 0;
+    }
+}
