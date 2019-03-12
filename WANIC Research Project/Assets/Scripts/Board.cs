2b349e0d40472abc74ef5f600745d3a60ac5ab64
Kyle-2 <48459007+Kyle-2@users.noreply.github.com>
2019-03-11 18:00:24 -0700

    GameMaster
    added

--- WANIC Research Project/Assets/Scripts/Board.cs
+++ WANIC Research Project/Assets/Scripts/Board.cs
@@ -6,12 +6,23 @@ using UnityEngine.UI;
 public class Board : MonoBehaviour {
 
     public Cell[,] GameBoard = new Cell[4, 4];
-    public Button sd;
-	// Use this for initialization
-	void Start () {
+    Board instance;
+    private void Awake()
+    {
+        instance = this;
+    }
+    public static Board GetInstance()
+    { return instance; }
+    // Use this for initialization
+    void Start () {
         foreach (Cell c in GameBoard)
         {
             c.init();
         }
+    }
+
+    public Cell[,] Get ()
+    {
+        return GameBoard;
     }
 }
---
+++ WANIC Research Project/Assets/Scripts/GameManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class GameManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void Check ()
+    {
+        int[,] board2 = new int[4,4];
+        for (int i = 0; i < 4; i++)
+        {
+            for (int j = 0; j < 4; j++)
+            {
+                if(Board.GetInstance().Get()[i, j].High != 0)
+                {
+                    board2[i, j] = Board.GetInstance().Get()[i, j].High;
+                }
+                else if(Board.GetInstance().Get()[i, j].Medium != 0)
+                {
+                    board2[i, j] = Board.GetInstance().Get()[i, j].Medium;
+                }
+                else
+                {
+                    board2[i, j] = Board.GetInstance().Get()[i, j].Low;
+                }
+            }
+        }
+
+
+
+    }
+}
