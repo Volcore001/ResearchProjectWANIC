42f7f716a1a3d12a0ad2f9b51eb8fdab64a63c31
William Siauw <36526721+Volcore001@users.noreply.github.com>
2019-03-12 15:45:40 -0700

    Fixed Board.cs

--- WANIC Research Project/Assets/Scripts/Board.cs
+++ WANIC Research Project/Assets/Scripts/Board.cs
@@ -1,12 +1,9 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.UI;
+﻿using UnityEngine;
 
 public class Board : MonoBehaviour {
 
     public Cell[,] GameBoard = new Cell[4, 4];
-    Board instance;
+    static Board instance;
     private void Awake()
     {
         instance = this;
