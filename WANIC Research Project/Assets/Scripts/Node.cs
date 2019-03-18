59a13e1d2a3cb89c2c5e064c4cee4b00cd6fa2c4
Kyle-2 <48459007+Kyle-2@users.noreply.github.com>
2019-03-18 16:01:19 -0700

    HERE

--- WANIC Research Project/Assets/Scripts/Node.cs
+++ WANIC Research Project/Assets/Scripts/Node.cs
@@ -10,18 +10,81 @@ public class Node
     List<Node> ChildrenNodes;
     public Node ParentNode;
     int Playerturn;
+    int MaxIndex;
 
     int P1 = 1;
     int P2 = 2;
     int P0 = 0;
 
-
+    public int PlayerOne()
+    {
+        return PlayerOneWin;
+    }
+    public int PlayerTwo()
+    {
+        return PlayerTwoWin;
+    }
     public void Tree ()
     {
         this.intialize();
         this.Branches();
         Debug.Log("Player One Wins: " + PlayerOneWin);
         Debug.Log("Player Two Wins: " + PlayerTwoWin);
+        if(PlayerOneWin >= PlayerTwoWin)
+        {
+            Debug.Log("Player One Win Chance: " + (PlayerOneWin/(PlayerOneWin+PlayerTwoWin)));
+        }
+        else
+        {
+            Debug.Log("Player Two Win Chance: " + (PlayerTwoWin / (PlayerOneWin + PlayerTwoWin)));
+        }
+                
+    }
+    void CominatorialGame ()
+    {
+        int Index = -1;
+        int WinRatio = 0;
+        
+       for (int i = 0; i <= this.MaxIndex; i++)
+       {
+            if (this.GetNode(i).PlayerOneWin != 0 && this.GetNode(i).PlayerTwoWin != 0)
+            {
+                if (this.Playerturn == P1)
+                {
+                    if (WinRatio < (this.GetNode(i).PlayerOneWin / this.GetNode(i).PlayerTwoWin))
+                    {
+                        WinRatio = (this.GetNode(i).PlayerOneWin / this.GetNode(i).PlayerTwoWin);
+                        Index = i;
+                    }
+                } else if (this.Playerturn == P2)
+                {
+                    if (WinRatio < (this.GetNode(i).PlayerTwoWin / this.GetNode(i).PlayerOneWin))
+                    {
+                        WinRatio = (this.GetNode(i).PlayerTwoWin / this.GetNode(i).PlayerOneWin);
+                        Index = i;
+                    }
+                }
+            }
+      }
+      if (Index == -1)
+      {
+          if (this.PlayerOneWin == 1)
+          {
+              Debug.Log("Player One Win");
+          }
+          else if (this.PlayerTwoWin == 1)
+          {
+              Debug.Log("Player Two Win");
+          }
+          else
+          {
+              Debug.Log("Tie");
+          }
+      } else
+      {
+          this.GetNode(Index).CominatorialGame();
+      }
+      
     }
     void Branches ()
     {
@@ -38,12 +101,12 @@ public class Node
                     this.AddChildNode();
                     check++;
                 }
-                if ((CurBoard[i, j].High == 0) && (CurBoard[i, j].Medium == 0))
+                if ((CurBoard[i, j].High == P0) && (CurBoard[i, j].Medium == P0))
                 {
                     this.AddChildNode();
                     check++;
                 }
-                if ((CurBoard[i, j].High == 0) && (CurBoard[i, j].Medium == 0) && (CurBoard[i, j].Low == 0))
+                if ((CurBoard[i, j].High == P0) && (CurBoard[i, j].Medium == P0) && (CurBoard[i, j].Low == P0))
                 {
                     this.AddChildNode();
                     check++;
@@ -57,35 +120,35 @@ public class Node
             for (int j = 0; j < 4; ++j)
             {
                 //low
-                if ((CurBoard[i, j].High == 0) && (CurBoard[i, j].Medium == 0) && (CurBoard[i, j].Low == 0))
+                if ((CurBoard[i, j].High == P0) && (CurBoard[i, j].Medium == P0) && (CurBoard[i, j].Low == P0))
                 {
                     index++;
                     //Debug.Log("Low [" + i + ", " + j + "]");
-                    if (this.GetNode(index).Playerturn == 1)
+                    if (this.GetNode(index).Playerturn == P1)
                     {
-                        this.GetNode(index).Playerturn = 2;
-                        this.GetNode(index).CurBoard[i, j].Low = 1;
+                        this.GetNode(index).Playerturn = P2;
+                        this.GetNode(index).CurBoard[i, j].Low = P1;
                     }
                     else
                     {
-                        this.GetNode(index).Playerturn = 1;
-                        this.GetNode(index).CurBoard[i, j].Low = 2;
+                        this.GetNode(index).Playerturn = P1;
+                        this.GetNode(index).CurBoard[i, j].Low = P2;
                     }
                 }
                 //Med
-                if ((CurBoard[i, j].High == 0) && (CurBoard[i, j].Medium == 0))
+                if ((CurBoard[i, j].High == P0) && (CurBoard[i, j].Medium == P0))
                 {
                     index++;
                     //Debug.Log("Medium [" + i + ", " + j + "]");
-                    if (this.GetNode(index).Playerturn == 1)
+                    if (this.GetNode(index).Playerturn == P1)
                     {
-                        this.GetNode(index).Playerturn = 2;
-                        this.GetNode(index).CurBoard[i, j].Medium = 1;
+                        this.GetNode(index).Playerturn = P2;
+                        this.GetNode(index).CurBoard[i, j].Medium = P1;
                     }
                     else
                     {
-                        this.GetNode(index).Playerturn = 1;
-                        this.GetNode(index).CurBoard[i, j].Medium = 2;
+                        this.GetNode(index).Playerturn = P1;
+                        this.GetNode(index).CurBoard[i, j].Medium = P2;
                     }
                 }
                 //High
@@ -111,6 +174,7 @@ public class Node
         //WARNING
         if (index == check)
         {
+            this.MaxIndex = index;
             for (int i = 0; i <= index; ++i)
             {
                 if (Cell.Check(CurBoard) == P0)
